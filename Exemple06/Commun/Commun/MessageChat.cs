d8e8058a949116e555aa45712de695fc75f6659d
Michel DIEMER <michel.diemer@yahoo.fr>
2022-04-22 08:58:19 +0200

    typeMessageReseau

---
+++ Exemple06/Commun/Commun/MessageChat.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Text;
+
+namespace Commun
+{
+    public class MessageChat
+    {
+        private static char separateur = '#';
+
+        private IPAddress ipEmetteur;
+        public IPAddress IpEmetteur
+        {
+            get { return ipEmetteur; }
+            set { ipEmetteur = value; }
+        }
+
+        private char typeMessage;
+        public char TypeMessage
+        {
+            get { return typeMessage; }
+        }
+
+        private string texte;
+        public string Texte
+        {
+            get { return texte; }
+            set { texte = value; }
+        }
+        public MessageChat(char p_typeMessage, IPAddress p_ipEmetteur, string p_texte)
+        {
+            typeMessage = p_typeMessage;
+            ipEmetteur = p_ipEmetteur;
+            texte = p_texte;
+        }
+        public byte[] GetInfos()
+        {
+            string infos = typeMessage.ToString() + separateur
+                            + ipEmetteur.ToString() + separateur.ToString()
+            + texte + separateur.ToString();
+            return Encoding.Unicode.GetBytes(infos);
+        }
+        public MessageChat(byte[] p_infos)
+        {
+            string infos = Encoding.Unicode.GetString(p_infos);
+            string[] tabInfos = infos.Split(new char[] { separateur });
+            typeMessage = tabInfos[0][0];
+            ipEmetteur = IPAddress.Parse(tabInfos[1]);
+            texte = tabInfos[2];
+        }
+    }
+}
---
+++ Exemple06/Commun/Commun/UtilIP.cs
@@ -0,0 +1,27 @@
+﻿namespace Commun
+{
+
+    using System.Net;
+    using System.Net.Sockets;
+
+    public class UtilIP
+    {
+
+        public const UInt16 DEFAULT_PORT = 33000;
+        public static IPAddress GetAdrIpLocaleV4()
+        {
+            string hote = Dns.GetHostName();
+            IPHostEntry ipLocales = Dns.GetHostEntry(hote);
+            foreach (IPAddress ip in ipLocales.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip;
+                }
+            }
+            return IPAddress.Parse("127.0.0.1");
+            
+        }
+    }
+    
+}
